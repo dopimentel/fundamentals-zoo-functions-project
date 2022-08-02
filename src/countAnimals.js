6f823a139b1e60e2f82f821b800a83fe40f3d997
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-02 17:11:13 -0300

    Requisito 5 - Teste 1

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+
 }
 
 module.exports = countAnimals;
+
