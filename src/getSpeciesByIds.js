89f33c1e9b185263f975f9e06d6548e94394aef6
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-02 01:22:13 -0300

    Requisito 1 - refatorado e testado no Lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,11 +1,7 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (...ids) => {
-  if (!ids) {
-    return [];
-  }
-  return ids.map((id) => species.filter((specie) => specie.id === id).reduce((specie) => specie));
-};
+const getSpeciesByIds = (...ids) => (!ids ? [] : ids
+  .map((id) => species.filter((specie) => specie.id === id).reduce((specie) => specie)));
 
 module.exports = getSpeciesByIds;
