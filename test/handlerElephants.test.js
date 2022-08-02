afe439a8add67acb34d116d878926395e5398a0f
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-02 19:47:54 -0300

    Requisito 6 - Testa o retorno para count

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -4,4 +4,7 @@ describe('Testes da função HandlerElephants', () => {
   it('Testa se a função retorna null para parametro não especificado', () => {
     expect(handlerElephants('sdsdsc')).toBeNull();
   });
+  it('Testa se "count" retorna 4', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
 });
