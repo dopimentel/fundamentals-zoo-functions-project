7bd9db1089929dea56458e66a4229e98fe22794f
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-02 19:53:59 -0300

    Requisito 6 - Testa se averageAge retorna a média

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,7 +1,7 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
-  it('Testa se a função retorna null para parametro não especificado', () => {
+  it('Testa se a função retorna "null" para parametro não especificado', () => {
     expect(handlerElephants('sdsdsc')).toBeNull();
   });
   it('Testa se "count" retorna 4', () => {
@@ -9,5 +9,8 @@ describe('Testes da função HandlerElephants', () => {
   });
   it('Testa se "name" retorna um array que contém um elemento "Jeferson"', () => {
     expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('Testa se "averageAge" retorna a média', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
   });
 });
