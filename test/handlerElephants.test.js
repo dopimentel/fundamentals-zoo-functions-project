bc1f3ea1f1c6b4919ee8488b725744d02f996af8
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-02 19:10:07 -0300

    Requisito 6 - Testa retorno null para strings aleatórias

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,7 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Testa se a função retorna null para parametro não especificado', () => {
+    expect(handlerElephants('sdsdsc')).toBeNull();
+  });
+});
