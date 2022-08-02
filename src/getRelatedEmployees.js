044a45e2b2c4b4eda626a16d171ba939149d03e5
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-02 13:53:16 -0300

    Requisito 4 - Teste 3

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,11 +2,25 @@ const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  return employees.some((employee) => employee.managers.some((managerId) => managerId === id));
+  return employees.some((employee) => employee.managers.some((manager) => manager === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const manageredList = employees
+    .filter((employee) => employee.managers
+    .some((manager) => manager === managerId));
+  return manageredList.map((managered) => `${managered.firstName} ${managered.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
+
+// console.log(employees.filter((employee) => {
+//   return employee.managers.some((managerId) => managerId === "9e7d4524-363c-416a-8759-8aa7e50c0992")}))
+
+
+// const gerenciados = employees.filter((employee) => {
+//   return employee.managers.some((managerId) => managerId === "9e7d4524-363c-416a-8759-8aa7e50c0992")});
+
+// console.log(gerenciados.map((gerenciado) => `${gerenciado.firstName} ${gerenciado.lastName}`))
+
+console.log(getRelatedEmployees("9e7d4524-363c-416a-8759-8aa7e50c0992"));
