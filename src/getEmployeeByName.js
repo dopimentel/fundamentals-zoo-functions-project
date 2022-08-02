72bcba8a80fbef25be6d3dfe2c93eeef4bcae5c3
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-02 02:25:17 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,8 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const getEmployeeByName = (employeeName) => (!employeeName ? {} : employees
+  .filter((employee) => employee.firstName === employeeName || employee.lastName === employeeName)
+  .reduce((employee) => employee));
 
 module.exports = getEmployeeByName;
