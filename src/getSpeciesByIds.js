707c8c020748406db55531be5666e26edbd9e4ea
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-02 01:14:29 -0300

    Requisito 1 - teste 3

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,12 +1,11 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
+const getSpeciesByIds = (...ids) => {
   if (!ids) {
     return [];
   }
-  return species.filter((specie) => specie.id === ids);
+  return ids.map((id) => species.filter((specie) => specie.id === id).reduce((specie) => specie));
 };
 
 module.exports = getSpeciesByIds;
-console.log(getSpeciesByIds("0938aa23-f153-4937-9f88-4858b24d6bce"));
