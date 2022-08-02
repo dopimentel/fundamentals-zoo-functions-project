467ff844a4f64a02cabc6fbd0bc327951627b1ca
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-02 01:54:34 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const getAnimalsOlderThan = (animal, age) =>
+  species
+    .filter((specie) => specie.name === animal)
+    .reduce((specie) => specie)
+    .residents.every((resident) => resident.age >= age);
 
 module.exports = getAnimalsOlderThan;
