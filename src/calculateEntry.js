7661ae232fe8833689659cd0960f4389e8a1f4b5
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-03 02:39:52 -0300

    Requisito 8 - teste 1

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,8 +1,23 @@
+const { name } = require('faker/locale/pt_BR');
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const countEntrants = (entrants) => {
+  return entrants.reduce(
+    (acc, curr) => {
+      if (curr.age < 18) {
+        acc.child += 1;
+        return acc;
+      }
+      if (curr.age < 50) {
+        acc.adult += 1;
+        return acc;
+      }
+      acc.senior += 1;
+      return acc;
+    },
+    { adult: 0, child: 0, senior: 0 },
+  );
+};
 
 function calculateEntry(entrants) {
   // seu código aqui
