2e32f1cdde900a5ad1fc615addf60517cf3803d9
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-03 03:22:00 -0300

    Requisito 8 - Lint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,23 +1,21 @@
 const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  return entrants.reduce(
-    (acc, curr) => {
-      if (curr.age < 18) {
-        acc.child += 1;
-        return acc;
-      }
-      if (curr.age < 50) {
-        acc.adult += 1;
-        return acc;
-      }
-      acc.senior += 1;
+const countEntrants = (entrants) => entrants.reduce(
+  (acc, curr) => {
+    if (curr.age < 18) {
+      acc.child += 1;
       return acc;
-    },
-    { adult: 0, child: 0, senior: 0 },
-  );
-};
+    }
+    if (curr.age < 50) {
+      acc.adult += 1;
+      return acc;
+    }
+    acc.senior += 1;
+    return acc;
+  },
+  { adult: 0, child: 0, senior: 0 },
+);
 
 function calculateEntry(entrants) {
   if (!entrants || Object.keys(entrants).length === 0) {
