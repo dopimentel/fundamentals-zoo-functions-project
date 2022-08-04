a5112226564172be446d6c364c7a57ef46d1c06d
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-04 03:23:35 -0300

    Requisito 9 - Teste 2

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,38 @@
-const { species } = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getSchedule = (scheduleTarget) => species
-  .filter((specie) => specie.name === scheduleTarget).reduce((specie) => specie).availability;
+const exhibition = (weekDay) => {
+  if (weekDay === 'Monday') {
+    return 'The zoo will be closed!';
+  }
+  return species
+    .filter((specie) => specie.availability.some((day) => day === weekDay))
+    .reduce((availableAnimals, specie) => {
+      availableAnimals.push(specie.name);
+      return availableAnimals;
+    }, []);
+};
+const officeHour = (weekDay) => {
+  if (weekDay === 'Monday') {
+    return 'CLOSED';
+  }
+  return `Open from ${hours[`${weekDay}`].open}am until ${
+    hours[`${weekDay}`].close
+  }pm`;
+};
 
+const getSchedule = (scheduleTarget) => {
+  if (!scheduleTarget) {
+    return Object.keys(hours).reduce((acc, weekDay) => {
+      acc[weekDay] = {
+        officeHour: officeHour(weekDay),
+        exhibition: exhibition(weekDay),
+      };
+      return acc;
+    }, {});
+  }
+  return species
+    .filter((specie) => specie.name === scheduleTarget)
+    .reduce((specie) => specie).availability;
+}
 module.exports = getSchedule;
