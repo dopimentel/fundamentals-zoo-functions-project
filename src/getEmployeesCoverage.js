f77a281b848c5da6ccc69823c73d696147076dee
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-06 18:25:13 -0300

    Requisito 11 - Teste 1 e 2

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,21 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+const getSpeciesByIds = require('./getSpeciesByIds');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeesCoverage({ name, id}) {
+  const employeeInfo = employees.find(
+    (employee) =>
+      employee.firstName === name || employee.lastName === name,
+  );
+  const { id: employeeId, firstName, lastName, responsibleFor } = employeeInfo;
+  const fullName = `${firstName} ${lastName}`;
+  const speciesNames = responsibleFor.map(
+    (specieId) => getSpeciesByIds(specieId).reduce((specie) => specie).name
+  );
+  const locations = responsibleFor.map(
+    (specieId) => getSpeciesByIds(specieId).reduce((specie) => specie).location
+  );
+  return { id: employeeId, fullName, species: speciesNames, locations };
 }
 
 module.exports = getEmployeesCoverage;
