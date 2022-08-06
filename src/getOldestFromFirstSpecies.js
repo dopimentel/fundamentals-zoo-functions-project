22c1aa64d8b6dc44457ae744ea236e70cb7bf161
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-06 16:35:29 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstSpecie = employees
+    .filter((employee) => employee.id === id)
+    .reduce((employee) => employee)
+    .responsibleFor.find((firstSpecieId) => firstSpecieId !== undefined);
+  const firstSpecieResidents = species
+    .filter((specie) => specie.id === firstSpecie)
+    .reduce((specie) => specie).residents;
+  const oldestResidentInfos = firstSpecieResidents
+    .reduce((oldest, resident) => ((oldest.age > resident.age) ? oldest : resident));
+  return Object.values(oldestResidentInfos);
 }
 
 module.exports = getOldestFromFirstSpecies;
