736c00dfc98f6a18660a1000aa41003bc7470590
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-06 18:32:42 -0300

    Requisito 11 - Teste 3

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -5,7 +5,7 @@ const getSpeciesByIds = require('./getSpeciesByIds');
 function getEmployeesCoverage({ name, id}) {
   const employeeInfo = employees.find(
     (employee) =>
-      employee.firstName === name || employee.lastName === name,
+      employee.firstName === name || employee.lastName === name || employee.id === id,
   );
   const { id: employeeId, firstName, lastName, responsibleFor } = employeeInfo;
   const fullName = `${firstName} ${lastName}`;
@@ -19,3 +19,24 @@ function getEmployeesCoverage({ name, id}) {
 }
 
 module.exports = getEmployeesCoverage;
+
+const employeeInfo = employees
+  .find((employee) => employee.firstName === 'Nigel' || employee.lastName === 'Nigel');
+
+const {id, firstName, lastName, responsibleFor} = employeeInfo;
+console.log(responsibleFor)
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+const speciesNames = responsibleFor.map((specieId) => getSpeciesByIds(specieId).reduce((specie) => specie).name);
+console.log(speciesNames);
+const locations = responsibleFor.map((specieId) => getSpeciesByIds(specieId).reduce((specie) => specie).location);
+console.log(locations);
+const resultado = { id, fullName, 'species': speciesNames, locations };
+console.log(resultado)
+// console.log(
+//   employees.find(
+//     (employee) =>
+//       employee.id === 'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'
+//   )
+// );
