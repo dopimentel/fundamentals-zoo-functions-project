6af711220ea4d1b13ee954c1b5b892243aa545ba
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-06 19:53:23 -0300

    Requisito 11 - Teste 4

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,41 +2,27 @@ const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 const getSpeciesByIds = require('./getSpeciesByIds');
 
-function getEmployeesCoverage({ name, id}) {
+const getEmployeeInfos = (employee) => {
+  const { id: employeeId, firstName, lastName, responsibleFor } = employee;
+  const fullName = `${firstName} ${lastName}`;
+  const speciesNames = responsibleFor.map(
+    (specieId) => getSpeciesByIds(specieId).reduce((specie) => specie).name,
+  );
+  const locations = responsibleFor.map(
+    (specieId) => getSpeciesByIds(specieId).reduce((specie) => specie).location,
+  );
+  return { id: employeeId, fullName, species: speciesNames, locations };
+};
+
+const getEmployeesCoverage = ({ name, id } = {}) => {
+  if (name === undefined && id === undefined) {
+    return employees.map((getEmployeeInfos));
+  }
   const employeeInfo = employees.find(
     (employee) =>
       employee.firstName === name || employee.lastName === name || employee.id === id,
   );
-  const { id: employeeId, firstName, lastName, responsibleFor } = employeeInfo;
-  const fullName = `${firstName} ${lastName}`;
-  const speciesNames = responsibleFor.map(
-    (specieId) => getSpeciesByIds(specieId).reduce((specie) => specie).name
-  );
-  const locations = responsibleFor.map(
-    (specieId) => getSpeciesByIds(specieId).reduce((specie) => specie).location
-  );
-  return { id: employeeId, fullName, species: speciesNames, locations };
-}
+  return getEmployeeInfos(employeeInfo);
+};
 
 module.exports = getEmployeesCoverage;
-
-const employeeInfo = employees
-  .find((employee) => employee.firstName === 'Nigel' || employee.lastName === 'Nigel');
-
-const {id, firstName, lastName, responsibleFor} = employeeInfo;
-console.log(responsibleFor)
-const fullName = `${firstName} ${lastName}`;
-console.log(fullName);
-
-const speciesNames = responsibleFor.map((specieId) => getSpeciesByIds(specieId).reduce((specie) => specie).name);
-console.log(speciesNames);
-const locations = responsibleFor.map((specieId) => getSpeciesByIds(specieId).reduce((specie) => specie).location);
-console.log(locations);
-const resultado = { id, fullName, 'species': speciesNames, locations };
-console.log(resultado)
-// console.log(
-//   employees.find(
-//     (employee) =>
-//       employee.id === 'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'
-//   )
-// );
