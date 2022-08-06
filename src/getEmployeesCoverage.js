d30bb5f3c89661b56485af64125ce691ad53112f
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-06 20:26:50 -0300

    Requisito 11 - Done

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,17 +1,17 @@
-const { employees, species } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 const getSpeciesByIds = require('./getSpeciesByIds');
 
 const getEmployeeInfos = (employee) => {
-  const { id: employeeId, firstName, lastName, responsibleFor } = employee;
+  const { id, firstName, lastName, responsibleFor } = employee;
   const fullName = `${firstName} ${lastName}`;
-  const speciesNames = responsibleFor.map(
+  const species = responsibleFor.map(
     (specieId) => getSpeciesByIds(specieId).reduce((specie) => specie).name,
   );
   const locations = responsibleFor.map(
     (specieId) => getSpeciesByIds(specieId).reduce((specie) => specie).location,
   );
-  return { id: employeeId, fullName, species: speciesNames, locations };
+  return { id, fullName, species, locations };
 };
 
 const getEmployeesCoverage = ({ name, id } = {}) => {
@@ -22,6 +22,9 @@ const getEmployeesCoverage = ({ name, id } = {}) => {
     (employee) =>
       employee.firstName === name || employee.lastName === name || employee.id === id,
   );
+  if (employeeInfo === undefined) {
+    throw new Error('Informações inválidas');
+  }
   return getEmployeeInfos(employeeInfo);
 };
 
