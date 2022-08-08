44fd64e3414babc4d1d073bcafd990e039384fae
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-08 00:52:04 -0300

    Requisito 14 - Teste 1

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,17 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const getAnimalsByLocation = (location) => species
+  .filter((specie) => specie.location === location)
+  .map((specie) => specie.name);
+
 function getAnimalMap(options) {
-  // seu código aqui
+  return {
+    NE: getAnimalsByLocation('NE'),
+    NW: getAnimalsByLocation('NW'),
+    SE: getAnimalsByLocation('SE'),
+    SW: getAnimalsByLocation('SW'),
+  };
 }
 
 module.exports = getAnimalMap;
