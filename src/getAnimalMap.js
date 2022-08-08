e3de22075a3633c0e691300d62768849cdb98400
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-08 10:43:38 -0300

    Requisito 14 - Testes 2, 3 e 4

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -4,14 +4,33 @@ const data = require('../data/zoo_data');
 const getAnimalsByLocation = (location) => species
   .filter((specie) => specie.location === location)
   .map((specie) => specie.name);
+const getResidentsBySpecie = (param) => species
+  .filter((specie) => specie.name === param)
+  .reduce((specie) => specie).residents;
+const residentsNames = (param, callback) => callback(param).reduce((acc, resident) => {
+  acc.push(resident.name);
+  return acc;
+}, []);
+getResidentsBySpecie('lions');
 
-function getAnimalMap(options) {
-  return {
-    NE: getAnimalsByLocation('NE'),
-    NW: getAnimalsByLocation('NW'),
-    SE: getAnimalsByLocation('SE'),
-    SW: getAnimalsByLocation('SW'),
-  };
+const nameIncluser = (arrayAnimals, ordered) =>
+  arrayAnimals.map((animal) => {
+    const resultado = {};
+    resultado[animal] = ordered
+      ? residentsNames(animal, getResidentsBySpecie).sort()
+      : residentsNames(animal, getResidentsBySpecie);
+    return resultado;
+  });
+
+function getAnimalMap(options = {}) {
+  const { includeNames, sex, sorted } = options;
+  const arrayLocations = ['NE', 'NW', 'SE', 'SW'];
+  return arrayLocations.reduce((acc, curr) => {
+    acc[curr] = includeNames
+      ? nameIncluser(getAnimalsByLocation(curr))
+      : getAnimalsByLocation(curr);
+    return acc;
+  }, {});
 }
 
 module.exports = getAnimalMap;
